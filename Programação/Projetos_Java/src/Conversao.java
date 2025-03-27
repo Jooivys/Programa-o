5d08b4b24424739e8e223c8ac00cfbda9607c7d5
João Ivys <168938127+Jooivys@users.noreply.github.com>
2025-03-27 09:10:10 -0300

    Pao de batata

---
+++ Programação/Projetos_Java/src/Conversao.java
@@ -0,0 +1,12 @@
+public class Conversao {
+    public static void main(String[] args) {
+        // De double para int
+        double d = 1.23;
+        int i =  (int)d;
+        System.out.println(i);
+        // De int para double
+        int number = 23;
+        double doubl = number;
+        System.out.println(doubl);
+    }
+}
---
+++ Programação/Projetos_Java/src/Testes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ class Aluno{
+     public static String nome;  // Uso de static para
+     public static double AV1;
+     public static double AV2;
+     public static double CalcularMedia(){
+         return (AV1 + AV2 ) / 2;
+     }
+ }
+public class Testes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escreva seu nome: ");
+        Aluno.nome = sc.nextLine();
+        System.out.println("Escreva a primeira nota: ");
+        Aluno.AV1 = sc.nextDouble();
+        System.out.println("Escreva a segunda nota: ");
+        Aluno.AV2 = sc.nextDouble();
+        System.out.println(Aluno.nome +" sua média será "+Aluno.CalcularMedia());
+        sc.close();
+    }
+}
