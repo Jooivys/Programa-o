c9bfcfcd29e54b23e5147d1f4386b9816a1840d6
João Ivys <168938127+Jooivys@users.noreply.github.com>
2025-04-12 11:14:40 -0300

    Alguns códigos
    Alguns códigos

--- Programação/Projetos_Java/src/MediaAluno.java
+++ Programação/Projetos_Java/src/MediaAluno.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
  class Aluno{
-     protected String nome;  // Uso de static para
      protected double AV1;
      protected double AV2;
-     protected double CalcularMedia(){
-         return (AV1 + AV2 ) / 2;
+     protected double AV3;
+     public final double media(){
+         return (AV1 + AV2 + AV3) / 2;
      }
  }
 public class MediaAluno {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Aluno Aluno = new Aluno();
-        System.out.println("Escreva seu nome: ");
-        Aluno.nome = sc.nextLine();
+        Aluno aluno = new Aluno();
         System.out.println("Escreva a primeira nota: ");
-        Aluno.AV1 = sc.nextDouble();
+        aluno.AV1 = sc.nextDouble();
         System.out.println("Escreva a segunda nota: ");
-        Aluno.AV2 = sc.nextDouble();
-        System.out.println(Aluno.nome+ " sua média será " +Aluno.CalcularMedia());
-        sc.close();
+        aluno.AV2 = sc.nextDouble();
+        System.out.println("Escreva a terceira nota: ");
+        aluno.AV3 = sc.nextDouble();
+
+        if (aluno.media() >= 7){
+            System.out.println("Você está APROVADO ");
+            System.out.println("Sua nota é "+aluno.media());
+        } else {
+            System.out.println("Você está REPROVADO");
+            System.out.println("Sua nota é "+aluno.media());
+        }
     }
 }
