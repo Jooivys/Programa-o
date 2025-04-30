6bf4b19911caa0f842ffcc79792787a6a805a1ff
Jooivys <ivysjoao135@gmail.com>
2025-04-30 08:41:45 -0300

    Exclusao da pasta java

--- Programação/Projetos_Java/src/HelloWord.java
+++
@@ -1,6 +0,0 @@
-public class HelloWord {
-    public static void main(String[] args) {
-        String sayHello = "Hello Word";
-        System.out.println(sayHello);
-    }
-}
--- Programação/Projetos_Java/src/Operadores.java
+++
@@ -1,8 +0,0 @@
-// AND = &&
-//OR = ||
-// NOT = !
-public class Operadores {
-    public static void main(String[] args) {
-
-    }
-}
